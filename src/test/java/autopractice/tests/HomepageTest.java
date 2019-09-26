2003240d9da135e4647db4328d452b0f58fe3384
OnchariWycliffe <onchariwycliffe.qa@gmail.com>
2019-09-26 18:44:13 +0300

    added support for extent reports

--- src/test/java/autopractice/tests/HomepageTest.java
+++ src/test/java/autopractice/tests/HomepageTest.java
@@ -1,25 +1,50 @@
 package autopractice.tests;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 import autpractice.pages.HomePage;
 import autpractice.util.PropertyManager;
 
 
 public class HomepageTest extends BaseTest {
 	
-   
-    
+     
     @Test(priority=1)
     public void launchApplication() {
+    	ExtentHtmlReporter extentHtmlReporter = new ExtentHtmlReporter("./ExtentReports/homepageTest.html");
+    	
+    	ExtentReports extentReports = new ExtentReports();
+    	extentReports.attachReporter(extentHtmlReporter);
+    	
+    	ExtentTest test = extentReports.createTest("Home page Test", "This test is meant to test all the functionality in the homepage");
+    	
+    	test.log(Status.INFO, "Opening the application Url");
         driver.get(PropertyManager.getInstance().getUrl());
+        
+        test.pass("Successfully opened the AUT url");
+        
         home = new HomePage(driver);
-        home.clickLoginLink();   
+        try {
+			test.addScreenCaptureFromPath("screenshot.png");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        test.log(Status.INFO, "Clicking Signin button");
+        
+        
+        home.clickLoginLink();  
+        
+        extentReports.flush();
     }
-    
-  
-    
-    
- 
 
   
 }
